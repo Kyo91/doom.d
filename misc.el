64ab81082c318fc04e81cff0908e3cff597ba225
Sam Blumenthal <sam.sam.42@gmail.com>
2022-05-08 09:41:34 -0500

    Optional TODO

--- misc.el
+++ misc.el
@@ -4,17 +4,19 @@
 (defun kyo/default-project-agenda-text (project-name)
   (concat "#+TITLE: " project-name "\n* " (capitalize project-name) " Items"))
 
-(defun kyo//open-project-agenda (project-name)
+(defun kyo//open-project-agenda (project-name &optional insert-todo)
   (let* ((file-name (concat "project-" project-name ".org"))
          (path (doom-path org-directory file-name))
          (text (kyo/default-project-agenda-text project-name)))
     (if (file-exists-p path)
         (progn
-          (find-file path)
-          (org-insert-todo-heading nil))
+          (find-file path))
       (progn
         (find-file path)
-        (insert text)))))
+        (insert text)))
+    (when insert-todo
+        (org-insert-todo-heading nil))))
 
-(defun kyo/open-project-agenda () (interactive)
-  (kyo//open-project-agenda (projectile-project-name)))
+(defun kyo/open-project-agenda ()
+  (interactive)
+  (kyo//open-project-agenda (projectile-project-name) (yes-or-no-p "Create todo?")))
